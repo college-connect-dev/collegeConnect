1d3e86aa6599790a02b3773b2aa9cba76c215b1e
adityagantayat <adityanarayan10@gmail.com>
2022-08-22 21:45:52 +0530

    added comments

--- server/index.js
+++ server/index.js
@@ -6,11 +6,15 @@ import dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
+
+//set up middlewares
 app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 const PORT = process.env.PORT || 5000;
+
+//Connection to DB
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
